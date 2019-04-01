d4d66e92a338ffb36e7c5bc5270ab522c3786bed
tomer flom <flom.tomer@gmail.com>
2019-04-01 16:59:20 +0300

    stable device+desktop 1.0

--- raspi/callAPI/index.js
+++ raspi/callAPI/index.js
@@ -3,11 +3,14 @@ const device   =  {id: process.env.DEVICE_ID, password: process.env.DEVICE_PASSW
 
 const callAPI = (barcode) => {
   if(barcode.length<6) return false;
+  console.log(`barcode: <${barcode}>`);
   CALL_API('post', `/device/scan/${barcode}`, device).then((cb) => {
-    //console.log(cb);
-    console.log(`barcode: <${barcode}>`);
+    console.log(cb);  
   }).catch((e) => {
-    console.log(`scan failed: ${e.response.data}`);
+	if(e.response)
+    	console.log(`scan failed: ${e.response.data}`);
+    else
+		console.log(`API not availble on localhost`);
   })
 }
 
