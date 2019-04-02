7f6a3cca599c0892eda9cf4233b98f2c1768f2d1
tomer flom <flom.tomer@gmail.com>
2019-04-03 00:56:46 +0300

    fix https bug

--- raspi/callAPI/call_api.js
+++ raspi/callAPI/call_api.js
@@ -1,18 +1,24 @@
 const axios = require('axios');
+const https = require('https');
 
-const URI = (process.env.NODE_ENV==='desktop') ? 'http://localhost:9001' : 'http://montv10.net:9399'; //https://gulo-service.herokuapp.com
+const URI = (process.env.NODE_ENV==='desktop') ? 'http://localhost:9001' : 'https://montv10.net:9400'; //https://gulo-service.herokuapp.com
 console.log(URI);
 const API_CALL = (verb, path, data) => {
   return new Promise((resolve, reject) => {
     let url = `${URI}${path}`;
-
+	const agent = new https.Agent({
+		rejectUnauthorized: false
+	});
     axios(`${URI}${path}`, {
       method: verb,
       withCredentials: true,
-      data
+      data,
+		httpsAgent: agent
     }).then((res) => {
       resolve(res.data);
     }).catch((err) => {
+		console.log(err.message);
+		console.log("here")
       reject(err);
     })
   });
